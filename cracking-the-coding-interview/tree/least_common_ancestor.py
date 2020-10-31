87f016e93b5b4d88409b9956ddcdaa6e613846ff
crp3 <caiorochapstrlrd@gmail.com>
2020-10-31 05:01:54 -0300

    add tree default code and implements least common ancestor

--- cracking-the-coding-interview/tree/least_common_ancestor.py
+++ cracking-the-coding-interview/tree/least_common_ancestor.py
@@ -1,35 +1,40 @@
-class Node:
-    def __init__(self, value, left=None, right=None):
-        self.value = value
-        self.left = left
-        self.right = right
+from tree_node import create_default_tree
 
-    def insert(self, value):
-        if self.value < value:
-            if self.right:
-                self.right.insert(value)
-            else:
-                self.right = Node(value)
-        elif self.value > value:
-            if self.left:
-                self.left.insert(value)
-            else:
-                self.left = Node(value)
+def least_common_ancestor(root, value_one, value_two):
+    path_one = path(root, value_one)
+    path_two = path(root, value_two)
+    index = 0
 
-    def print_tree(self):
-        if self.left:
-            self.left.print_tree()
-        if self.right:
-            self.right.print_tree()
-        print(self.value, end=', ')
+    while index < len(path_one) and index < len(path_two):
+        if path_one[index] != path_two[index]:
+            break
+        result = path_one[index]
+        index += 1
 
+    return result
+    
+
+def path(root, value):
+
+    ''' this function assumes the value is always present inside the tree '''
+
+    path = []
+    node = root
+
+    while node.value != value:
+        path.append(node.value)
+        if value > node.value:
+            node = node.right
+        else:
+            node = node.left
+    
+    path.append(value)
+
+    return path
 
 if __name__ == '__main__':
-    tree = Node(5)
-    tree.insert(3)
-    tree.insert(2)
-    tree.insert(4)
-    tree.insert(7)
-    tree.insert(6)
-    tree.insert(8)
-    tree.print_tree()
+    tree = create_default_tree()
+    assert least_common_ancestor(tree, 0, 5) == 5
+    assert least_common_ancestor(tree, 0, 4) == 3
+    assert least_common_ancestor(tree, 8, 6) == 7
+    assert least_common_ancestor(tree, 2, 8) == 5
