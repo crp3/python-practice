fceb1c3ddee05d2ab70d0fd56f01849d8d569367
crp3 <caiorochapstrlrd@gmail.com>
2020-10-31 04:19:28 -0300

    add tree code for least common ancestor

---
+++ cracking-the-coding-interview/tree/least_common_ancestor.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+    def insert(self, value):
+        if self.value < value:
+            if self.right:
+                self.right.insert(value)
+            else:
+                self.right = Node(value)
+        elif self.value > value:
+            if self.left:
+                self.left.insert(value)
+            else:
+                self.left = Node(value)
+
+    def print_tree(self):
+        if self.left:
+            self.left.print_tree()
+        if self.right:
+            self.right.print_tree()
+        print(self.value, end=', ')
+
+
+if __name__ == '__main__':
+    tree = Node(5)
+    tree.insert(3)
+    tree.insert(2)
+    tree.insert(4)
+    tree.insert(7)
+    tree.insert(6)
+    tree.insert(8)
+    tree.print_tree()
