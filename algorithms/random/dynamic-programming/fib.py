fbeedbd2031f448347486ffa0a3a21735c184aaf
Caio Rocha <caiorochapstrlrd@gmail.com>
2021-04-14 20:18:45 -0300

    change fib to use cache

--- algorithms/random/dynamic-programming/fib.py
+++ algorithms/random/dynamic-programming/fib.py
@@ -10,7 +10,7 @@ def fib_memo(n, memo = {}):
   if n in memo:
     return memo[n]
   if n <= 1: return n
-  memo[n] = fib_memo(n-1) + fib_memo(n-2)
+  memo[n] = fib_memo(n-1, memo) + fib_memo(n-2, memo)
   return memo[n]
 
 if __name__ == '__main__':
