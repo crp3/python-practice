e6987d4ca8ef7dd65907bf3439292725e259683b
Caio Rocha <caiorochapstrlrd@gmail.com>
2021-04-14 21:46:49 -0300

    add constant memory version for string algorithms

--- algorithms/cracking-the-coding-interview/array/palindrome_permutation.py
+++ algorithms/cracking-the-coding-interview/array/palindrome_permutation.py
@@ -1,3 +1,4 @@
+'''O(n) memory and O(n) time'''
 def is_palindrome_permutation(string):
     max_values = 1
     counter_dict = dict()
@@ -14,6 +15,19 @@ def is_palindrome_permutation(string):
     
     return max_values >= 0
 
+'''O(1) memory and O(n) time'''
+def is_palindrome_permutation_constant_memory(string):
+    max_values = 1
+    counts = [0]*128
+
+    for char in string:
+        counts[ord(char)] += 1
+    
+    for count in counts:
+        if count % 2 == 1:
+            max_values -= 1
+    
+    return max_values >= 0
 
 if __name__ == '__main__':
     assert not is_palindrome_permutation('abacab')
@@ -24,3 +38,12 @@ if __name__ == '__main__':
     assert is_palindrome_permutation('acca')
     assert is_palindrome_permutation('aacc')
     assert is_palindrome_permutation('')
+
+    assert not is_palindrome_permutation_constant_memory('abacab')
+    assert is_palindrome_permutation_constant_memory('ababa')
+    assert not is_palindrome_permutation_constant_memory('lalllllnllllal')
+    assert not is_palindrome_permutation_constant_memory('ccaaaccc')
+    assert is_palindrome_permutation_constant_memory('lloooop')
+    assert is_palindrome_permutation_constant_memory('acca')
+    assert is_palindrome_permutation_constant_memory('aacc')
+    assert is_palindrome_permutation_constant_memory('')
--- algorithms/cracking-the-coding-interview/array/unique.py
+++ algorithms/cracking-the-coding-interview/array/unique.py
@@ -1,8 +1,28 @@
+'''this has O(n) memory complexity and O(n) time complexity'''
 def unique(elements):
     return len(elements) == len(set(elements))
 
+'''version with O(1) memory complexity'''
+def unique_constant_memory(elements):
+    chars = [0]*128
+    for element in elements:
+        chars[ord(element)] += 1
+    
+    for char in chars:
+        if char > 1:
+            return False
+
+    return True
+
+
 if __name__ == '__main__':
-    assert unique([1,2,3,4,5])
-    assert not unique([1,1,2,3,4,5])
-    assert unique([1,10,100])
-    assert not unique([1,10,100,100,1000])
+    assert unique('abcdef')
+    assert not unique('aabcdef')
+    assert unique('axz')
+    assert not unique('abbcd')
+    
+    assert unique_constant_memory('abcdef')
+    assert not unique_constant_memory('aabcdef')
+    assert unique_constant_memory('axz')
+    assert not unique_constant_memory('abbcd')
+ 
