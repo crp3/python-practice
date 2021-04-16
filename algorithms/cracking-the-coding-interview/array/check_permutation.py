d731b6817f8b3936e47fecda761de47b86953743
Caio Rocha <caiorochapstrlrd@gmail.com>
2021-04-16 11:08:03 -0300

    add constant memory solution for check_permutation

--- algorithms/cracking-the-coding-interview/array/check_permutation.py
+++ algorithms/cracking-the-coding-interview/array/check_permutation.py
@@ -1,3 +1,4 @@
+''' O(n) time and O(n) memory '''
 def check_permutation(string_one, string_two):
     counter_dict = dict()
     result = len(string_one) == len(string_two)
@@ -17,6 +18,22 @@ def check_permutation(string_one, string_two):
 
     return result
 
+''' O(n) time and O(1) memory '''
+def check_permutation_constant_memory(string_one, string_two):
+    counts = [0]*128
+    result = len(string_one) == len(string_two)
+
+    for character in string_one:
+        counts[ord(character)] += 1
+
+    for character in string_two:
+        counts[ord(character)] -= 1
+
+    for count in counts:
+        result = result and (count == 0)
+
+    return result
+
 if __name__ == '__main__':
     assert check_permutation('caio', 'oiac')
     assert not check_permutation('caio', 'oiab')
@@ -24,3 +41,10 @@ if __name__ == '__main__':
     assert check_permutation('banana', 'anaban')
     assert not check_permutation('apple', 'alapp')
     assert check_permutation('','')
+
+    assert check_permutation_constant_memory('caio', 'oiac')
+    assert not check_permutation_constant_memory('caio', 'oiab')
+    assert not check_permutation_constant_memory('caio', 'oiacm')
+    assert check_permutation_constant_memory('banana', 'anaban')
+    assert not check_permutation_constant_memory('apple', 'alapp')
+    assert check_permutation_constant_memory('','')
