781e3c518883836c6c666410d4fc7fdb95bec50e
Caio Rocha <caiorochapstrlrd@gmail.com>
2021-08-10 20:01:03 -0300

    add constant memory solution for duplicates

--- algorithms/byte-by-byte/find_duplicates.py
+++ algorithms/byte-by-byte/find_duplicates.py
@@ -7,8 +7,23 @@ def find_duplicates(array):
     seen.add(item)
   return list(solution)
 
+def find_duplicates_constant_memory(array):
+  solution = set()
+  for item in array:
+    index = abs(item)-1
+    
+    if array[index] < 0:
+      solution.add(abs(item))
+    else:
+      array[index] = -array[index]
+  return list(solution)
+
 if __name__ == '__main__':
   assert find_duplicates([1, 2, 3]) == []
   assert find_duplicates([1, 2, 2]) == [2]
   assert find_duplicates([3, 3, 3]) == [3]
   assert find_duplicates([2, 1, 2, 1]) == [1, 2]
+  assert find_duplicates_constant_memory([1, 2, 3]) == []
+  assert find_duplicates_constant_memory([1, 2, 2]) == [2]
+  assert find_duplicates_constant_memory([3, 3, 3]) == [3]
+  assert find_duplicates_constant_memory([2, 1, 2, 1]) == [1, 2]
